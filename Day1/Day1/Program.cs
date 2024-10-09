66b8585296268ab7575ce9c1c22c1d359a9feb4d
ahmedabougabal <ahmedabougabal@live.com>
2024-10-09 17:37:05 +0300

    feat: Implements a nested while loop to check for user's credetials and if true, he will be given his address

---
+++ Day1/Day1/Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Console.WriteLine("WELCOME TO .NET");
+
+string street = "23 Main Street";
+string city ;
+string district= "NY 10001 USA";
+
+Console.WriteLine("enter your name");
+string name=Console.ReadLine();;
+
+while (name != "Alex")
+{ 
+    Console.WriteLine("not alex, \n try again");
+    name =Console.ReadLine();
+    if (name == "alex")
+    {
+        Console.WriteLine("enter your city");
+        city=Console.ReadLine();
+        while(city != "new york")
+        { 
+            Console.WriteLine("not the right city, try again>> ");
+            city=Console.ReadLine();
+        } 
+        Console.WriteLine(name+" "+street+" "+city+" "+district); 
+        break;
+    }
+}
+
+
+
