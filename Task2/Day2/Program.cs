6156aeeaaa22f2d334a0ce04eafea7e306a1e93a
ahmedabougabal <ahmedabougabal@live.com>
2024-10-13 23:05:32 +0300

    Implementing Generics, Operator Overloading and type checking in C#

---
+++ Task2/Day2/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+namespace Day2;
+class Program
+{
+    static void Main(string[] args)
+    {
+        ClassRooms.TestStudents();
+    }
+}
