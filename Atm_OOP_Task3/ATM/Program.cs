cc20975f9d4b3944dc77dffddd46b52e8df46113
ahmedabougabal <ahmedabougabal@live.com>
2024-10-15 19:25:51 +0300

    Feat: Implement ATM System with Core Features
    - Added user sign-up functionality to register new users.
    - Implemented login functionality with validation for correct username and password.
    - Developed balance check feature to display current user balance.
    - Added deposit operation with input validation to ensure positive amounts.
    - Created withdrawal functionality with validation to ensure sufficient balance.
    - Included error handling for invalid inputs and incorrect login credentials.
    - Allowed multiple operations during a single session until user opts to exit.

---
+++ Atm_OOP_Task3/ATM/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+namespace ATM;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Atm atm = new();
+        atm.Run();
+    }
+}
