02823a289b483a7fc0413cd9886bafe48cf91005
ahmedabougabal <ahmedabougabal@live.com>
2024-10-16 15:41:49 +0300

    Feat: adds functionality to delete a registered user and the ability to record each transaction's date and time

---
+++ EnhancedAtmOop_Task4/Atm/Atm.cs
@@ -0,0 +1,215 @@
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Runtime.CompilerServices;
+
+namespace ATM;
+
+public class Transaction
+{
+    public DateTime Timestamp { get; set; }
+    public string Type { get; set; }
+    public decimal Amount { get; set; }
+
+    public Transaction(DateTime timestamp, string type, decimal amount)
+    {
+        Timestamp = timestamp;
+        Type = type;
+        Amount = amount;
+    }
+    
+}
+
+public class User
+{
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public decimal Balance { get; set; }
+    public List<Transaction> Transactions { get; set; }
+
+    public User(string username, string password, decimal balance)
+    {
+        Username = username;
+        Password = password;
+        Balance = 0;
+        Transactions = new List<Transaction>();
+    }
+}
+
+
+public class Atm
+{
+    private List<User> users = new List<User>();
+    private User currentUser;
+
+    public void Run()
+    {
+        while (true)
+        {
+            Console.WriteLine("1. Sign up");
+            Console.WriteLine("2. Login");
+            Console.WriteLine("3. Delete a user");
+            Console.WriteLine("4. Exit");
+            Console.WriteLine("Choose an option:");
+            string choice = Console.ReadLine();
+            switch (choice)
+            {
+                case "1":
+                    SignUp();
+                    break;
+                case "2":
+                    if (Login())
+                    {
+                        performOperations();
+                    };
+                    break;
+                case "3":
+                    DeleteUsers();
+                    break;
+                case "4":
+                    return;
+                default:
+                    Console.WriteLine("Invalid choice, please try again.");
+                    break;
+            }
+        }
+    }
+
+    private void SignUp()
+    {
+        Console.WriteLine("Enter username:");
+        string username = Console.ReadLine();
+        Console.WriteLine("Enter password:");
+        string password = Console.ReadLine();
+        
+        users.Add(new User(username, password, 0));
+        Console.WriteLine("User registration successful.");
+    }
+
+    private bool Login()
+    {
+        Console.WriteLine("Enter username:");
+        string username = Console.ReadLine();
+        Console.WriteLine("Enter password:");
+        string password = Console.ReadLine();
+        currentUser = users.FirstOrDefault(u => u.Username == username && u.Password == password);
+
+        if (currentUser != null)
+        {
+            Console.WriteLine("You have logged in.");
+            return true;
+        }
+        else
+        {
+            Console.WriteLine("Username or password is incorrect, try again.");
+            return false;
+        }
+    }
+
+
+    private void DeleteUsers()
+    {
+        Console.WriteLine("Enter username to delete:");
+        string username=Console.ReadLine();
+        Console.WriteLine("Enter password:");
+        string password=Console.ReadLine();
+        
+        User usertodelete = users.FirstOrDefault(u => u.Username == username && u.Password == password);
+        if (usertodelete != null)
+        {
+            users.Remove(usertodelete);
+            Console.WriteLine("User is deleted successfully.");
+        }
+        else
+        {
+            Console.WriteLine("Username or password is incorrect, user not found.");
+        }
+
+    }
+    
+    
+    
+    private void performOperations()
+    {
+        while (true)
+        {
+            
+        Console.WriteLine("\n 1. check balance");
+        Console.WriteLine("2. Make a deposit");
+        Console.WriteLine("3. Withdraw Balance");
+        Console.WriteLine("4. Exit");
+        Console.Write("choose an option: ");
+        string choice = Console.ReadLine();
+        switch (choice)
+        {
+            case "1":
+                checkBalance();
+                break;
+            case "2":
+                makeDeposit();
+                break;
+            case "3":
+                withdrawBalance();
+                break;
+            case "4":
+                return;
+            default:
+                Console.WriteLine("Invalid choice, please try again.");
+                break;
+        }
+        Console.WriteLine("\n Do you want to perform any other operations? (y/n)");
+        string answer = Console.ReadLine();
+        if (answer.ToLower() != "y")
+        {
+            break; 
+        } 
+        }
+
+        
+    }
+
+    private void checkBalance()
+    {
+        Console.WriteLine($"your current balance is {currentUser.Balance} ");
+        currentUser.Transactions.Add(new Transaction(DateTime.Now, "Balance checked ", currentUser.Balance));
+    }
+
+    private void makeDeposit()
+    {
+        Console.WriteLine("Enter the deposit amount:");
+        if (decimal.TryParse(Console.ReadLine(), out decimal amount) && amount > 0)
+        {
+            currentUser.Balance += amount; 
+            Console.WriteLine($"Your new balance is {currentUser.Balance} ");  
+            currentUser.Transactions.Add(new Transaction(DateTime.Now, "Deposit made ", amount));
+        }
+        else
+        {
+            Console.WriteLine("Invalid deposit amount, please enter a positive number.");
+        }
+    }
+
+    private void withdrawBalance()
+    {
+        Console.WriteLine("enter the desired withdraw amount:");
+        if (decimal.TryParse(Console.ReadLine(), out decimal amount) && amount > 0)
+        {
+            if (amount <= currentUser.Balance)
+            {
+                currentUser.Balance -= amount;
+                Console.WriteLine($"withdraw successful, Your balance is now {currentUser.Balance} ");
+                currentUser.Transactions.Add(new Transaction(DateTime.Now, "Withdraw made ", -amount));
+            }
+            else
+            {
+                Console.WriteLine("insufficient balance, your new balance is {0} ", currentUser.Balance);
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid withdrawing amount, please enter a positive number.");
+        }
+    }
+}
