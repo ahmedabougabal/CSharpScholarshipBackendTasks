84c0d2f6863b3e30df192c36895f04c73a7e1e45
ahmedabougabal <ahmedabougabal@live.com>
2024-10-27 13:30:13 +0300

    Feat: Implements a real ATM machine system based on a real life logic separating classes into directories, applying modularization and code reusability

---
+++ SimRealWorldAtmMachine/ATMApp/App/ATMApp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ATMApp.Domain.Entities;
+using ATMApp.UI;
+namespace ATMApp.App
+{
+    public class ATMApp
+    {
+        private List<UserAccount> userAccountlist;
+        private UserAccount SelectedAcccount;
+
+        public void InitializeData()
+        {
+            userAccountlist = new List<UserAccount>
+            {
+                new UserAccount{Id = 1, FullName="Ahmed Abou Gabal", AccountNumber=123456, CardNumber = 321321 , CardPin=2410,
+                AccountBalance= 10000, isLocked=false},
+                new UserAccount{Id = 2, FullName="Mr X", AccountNumber=12345678, CardNumber = 32132132 , CardPin=2410,
+                AccountBalance= 5000, isLocked=false},
+                new UserAccount{Id = 3, FullName="Mrs Y", AccountNumber=123456789, CardNumber = 321321321 , CardPin=2410,
+                AccountBalance= 700, isLocked=true}
+            };
+
+        }
+
+        
+    }
+}
---
+++ SimRealWorldAtmMachine/ATMApp/App/Entry.cs
@@ -0,0 +1,20 @@
+﻿using ATMApp.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMApp.App
+{
+    class Entry
+    {
+        static void Main(string[] args)
+        {
+            AppScreen.Welcome();
+            long CardNumber = Validator.Convert<long>("your card number");
+            Console.WriteLine($"your card number is {CardNumber}");
+            Utility.PressEnterToContinue();
+        }
+    }
+}
