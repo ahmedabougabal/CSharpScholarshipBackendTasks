b8a0fe6939231e00715afc208711c356fe32fd90
ahmedabougabal <ahmedabougabal@live.com>
2024-10-27 13:30:13 +0300

    Feat: Implements a real ATM machine system based on a real life logic separating classes into directories, applying modularization and code reusability

---
+++ SimRealWorldAtmMachine/ATMApp/UI/AppScreen.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMApp.UI
+{
+    public static class AppScreen
+    {
+        internal static void Welcome()
+        {
+
+            Console.Clear();
+            Console.Title = "My ATM App";
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("\n\n-------------Welcome to my ATM app-------------\n\n");
+            //ask the user for card number
+            Console.WriteLine("Please insert your ATM card");
+            Utility.PressEnterToContinue();
+        }
+
+   
+    }
+}
---
+++ SimRealWorldAtmMachine/ATMApp/UI/Utility.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATMApp.UI
+{
+    public static class Utility
+    {
+        public static void PrintMessage(string msg, bool success = true)
+        {
+            if (success) 
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+            }
+            else
+            {
+                Console.ForegroundColor= ConsoleColor.Red;
+            }
+            Console.WriteLine(msg);
+            Console.ForegroundColor= ConsoleColor.White;
+            PressEnterToContinue();
+
+        }
+        public static string GetuserInput(string prompt) 
+        {
+            Console.WriteLine($"Enter {prompt}");
+            return Console.ReadLine();
+        
+        }
+
+        public static void PressEnterToContinue()
+        {
+            Console.WriteLine("\n press enter to continue...");
+            Console.ReadLine();
+        }
+    }
+}
