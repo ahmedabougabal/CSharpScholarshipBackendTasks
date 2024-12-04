f1f95407f0a894fb424cd8bc095c547796ee7913
ahmedabougabal <ahmedabougabal@live.com>
2024-12-04 23:22:23 +0200

    feat(user): Extend AppUser model with additional fields
    - Add FirstName, LastName, Address fields
    - Add CreatedAt and LastLoginAt timestamps
    - Add proper MongoDB BSON attributes
    - Initialize timestamps in constructor
    - Add Cart and Orders collections

--- eCommerceWebApp/eCommerce/Models/AppUser.cs
+++ eCommerceWebApp/eCommerce/Models/AppUser.cs
@@ -1,19 +1,45 @@
 using Microsoft.AspNetCore.Identity;
 using System.Collections.Generic;
 using AspNetCore.Identity.MongoDbCore.Models;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using System;
 
 namespace eCommerce.Models
 {
+    [BsonIgnoreExtraElements]
     public class AppUser : MongoIdentityUser<string>
     {
         public AppUser()
         {
             Cart = new Cart();
             Orders = new List<Order>();
+            CreatedAt = DateTime.UtcNow;
+            LastLoginAt = DateTime.UtcNow;
         }
 
-        public int? CartId { get; set; }
+        [BsonElement("cart")]
         public Cart Cart { get; set; }
+
+        [BsonElement("orders")]
         public ICollection<Order> Orders { get; set; }
+
+        [BsonElement("firstName")]
+        public string FirstName { get; set; }
+
+        [BsonElement("lastName")]
+        public string LastName { get; set; }
+
+        [BsonElement("createdAt")]
+        public DateTime CreatedAt { get; set; }
+
+        [BsonElement("lastLoginAt")]
+        public DateTime LastLoginAt { get; set; }
+
+        [BsonElement("address")]
+        public string Address { get; set; }
+
+        [BsonElement("phoneNumber")]
+        public string PhoneNumber { get; set; }
     }
 }
