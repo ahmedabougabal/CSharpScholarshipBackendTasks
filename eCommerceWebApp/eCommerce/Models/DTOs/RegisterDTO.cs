650dbfb60ae558473a44d428134f283a02ea4c2f
ahmedabougabal <ahmedabougabal@live.com>
2024-12-04 23:23:37 +0200

    refactor(dto): Update RegisterDTO validation
    - Remove 'required' keyword from properties
    - Keep Required attribute for validation
    - Add StringLength validation for Password
    - Add validation for additional user fields

--- eCommerceWebApp/eCommerce/Models/DTOs/RegisterDTO.cs
+++ eCommerceWebApp/eCommerce/Models/DTOs/RegisterDTO.cs
@@ -6,17 +6,32 @@ namespace eCommerce.Models.DTOs
     {
         [Required]
         [EmailAddress]
-        public required string Email { get; set; }
+        public string Email { get; set; }
 
         [Required]
         [StringLength(100, MinimumLength = 6)]
-        public required string Password { get; set; }
+        public string Password { get; set; }
 
         [Required]
         [Compare("Password")]
-        public required string ConfirmPassword { get; set; }
+        public string ConfirmPassword { get; set; }
 
         [Required]
-        public required string UserName { get; set; }
+        [StringLength(50)]
+        public string UserName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [StringLength(200)]
+        public string Address { get; set; }
+
+        [Phone]
+        public string PhoneNumber { get; set; }
     }
 }
